82dc6a941788f1160b49f0695193c7f66e4e1d7a
gtorlai <gtorlai@uwaterloo.ca>
2020-05-14 17:52:11 -0400

    Gradients of Z tested and working

--- src/qpt.jl
+++ src/qpt.jl
@@ -1,19 +1,23 @@
 using LinearAlgebra
 using ITensors
+using Random
 
 struct QPT
-  #Kp::Int                    # Number of input states 
-  prep::String                # name tag for preparation states
-  P::Vector{ITensor}          # Vector of preparation states
-  #Km::Int                    # Numbe of measurement POVMs
-  povm::String                # name tag for measurement povms
-  M::Vector{ITensor}          # Vector of POVM measurements
-  N::Int                      # Number of qubits
-  chi::Int                    # Bond dimension
-  mpo::MPO                    # Matrix product operator
+  #Kp::Int            # Number of input states 
+  prep::String        # name tag for preparation states
+  P::Vector{ITensor}  # Vector of preparation states
+  #Km::Int            # Numbe of measurement POVMs
+  povm::String        # name tag for measurement povms
+  M::Vector{ITensor}  # Vector of POVM measurements
+  N::Int              # Number of qubits
+  chi::Int            # Bond dimension
+  mpo::MPO            # Matrix product operator
+  seed::Int           # Seed for RNG
 end
 
-function QPT(;prep::String,povm::String,N::Int,chi::Int)
+function QPT(;prep::String,povm::String,N::Int,chi::Int,seed::Int)
+  Random.seed!(seed);
+
   # Set the input states
   if prep == "Pauli6"
     #Kp = 6
@@ -63,14 +67,18 @@ function QPT(;prep::String,povm::String,N::Int,chi::Int)
   s_i   = [Index(d; tags="site_in, s_i=$s") for s in 1:N]
   s_o   = [Index(d; tags="site_out, s_o=$s") for s in 1:N]
   A = ITensor[]
-  push!(A,ITensor(rand(d,d,chi),s_i[1],s_o[1],link[1]))
+  sigma = 0.1
+  rand_mat = sigma*(ones(d,d,chi)-rand(d,d,chi))+im*sigma*(ones(d,d,chi)-rand(d,d,chi))
+  push!(A,ITensor(rand_mat,s_i[1],s_o[1],link[1]))
   for j in 2:N-1
-    push!(A,ITensor(rand(chi,d,d,chi),link[j-1],s_i[j],s_o[j],link[j]))
+    rand_mat = sigma*(ones(chi,d,d,chi)-rand(chi,d,d,chi))+im*sigma*(ones(chi,d,d,chi)-rand(chi,d,d,chi))
+    push!(A,ITensor(rand_mat,link[j-1],s_i[j],s_o[j],link[j]))
   end
-  push!(A,ITensor(rand(chi,d,d),link[N-1],s_i[N],s_o[N]))
+  rand_mat = sigma*(ones(chi,d,d)-rand(chi,d,d))+im*sigma*(ones(chi,d,d)-rand(chi,d,d))
+  push!(A,ITensor(rand_mat,link[N-1],s_i[N],s_o[N]))
   mpo = MPO(A)
 
-  return QPT(prep,P,povm,M,N,chi,mpo)
+  return QPT(prep,P,povm,M,N,chi,mpo,seed)
 
 end
 
@@ -100,7 +108,48 @@ function Normalization(mpo::MPO;choi::Bool=false)
 end;
 
 
-
+function GradientZ(qpt::QPT)
+    
+    Z = Normalization(qpt.mpo)
+    
+    R = ITensor[]
+    L = ITensor[]
+    
+    # Sweep right to get L
+    tensor =  qpt.mpo[1] * prime(dag(qpt.mpo[1]),"link")
+    push!(L,tensor)
+    for j in 2:N-1
+        tensor = L[j-1] * qpt.mpo[j];
+        tensor = tensor * prime(dag(qpt.mpo[j]),"link") 
+        push!(L,tensor)
+    end
+    
+    # Sweep left to get R
+    tensor = qpt.mpo[N] * prime(dag(qpt.mpo[N]),"link")
+    push!(R,tensor)
+    #for j in reverse(2:N-1)
+    for j in 2:N-1
+        tensor = R[j-1] * qpt.mpo[N+1-j]
+        tensor = tensor * prime(dag(qpt.mpo[N+1-j]),"link") 
+        push!(R,tensor)
+    end
+    
+    # Get the gradients of the normalization
+    gradients = ITensor[]
+    
+    # Site 1
+    tensor = qpt.mpo[1] * R[N-1]
+    push!(gradients,noprime(tensor)/Z)
+    
+    for j in 2:N-1
+        tensor = L[j-1] * qpt.mpo[j] * R[N-j]
+        push!(gradients,noprime(tensor)/Z)
+    end
+    tensor = L[N-1] * qpt.mpo[N]
+    push!(gradients,noprime(tensor)/Z)
+     
+    return 2*gradients
+end;
 
 
 
