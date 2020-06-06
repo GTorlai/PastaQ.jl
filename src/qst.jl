3ff4516d114ddc355a9c85526ddfa1a54fd7b6ca
gtorlai <torlaigiacomo@gmail.com>
2020-06-06 18:31:55 -0400

    Gradients OK for LPDO (w and w/o localnorms)

--- src/qst.jl
+++ src/qst.jl
@@ -55,9 +55,45 @@ function lognormalize!(M::Union{MPS,MPO})
   logZ += log(real(blob[]))
   return logZ,localnorms
 end
+"""
+Gradients of logZ 
+"""
+function gradlogZ(M::Union{MPS,MPO};localnorm=nothing)
+  N = length(M)
+  L = Vector{ITensor}(undef, N-1)
+  R = Vector{ITensor}(undef, N)
+  
+  if isnothing(localnorm)
+    localnorm = ones(N)
+  end
+  
+  # Sweep right to get L
+  L[1] = dag(M[1]) * prime(M[1],"Link")
+  for j in 2:N-1
+    L[j] = L[j-1] * dag(M[j])
+    L[j] = L[j] * prime(M[j],"Link")
+  end
+  Z = L[N-1] * dag(M[N])
+  Z = real((Z * prime(M[N],"Link"))[])
+
+  # Sweep left to get R
+  R[N] = dag(M[N]) * prime(M[N],"Link")
+  for j in reverse(2:N-1)
+    R[j] = R[j+1] * dag(M[j])
+    R[j] = R[j] * prime(M[j],"Link")
+  end
+  # Get the gradients of the normalization
+  gradients = Vector{ITensor}(undef, N)
+  gradients[1] = prime(M[1],"Link") * R[2]/(localnorm[1]*Z)
+  for j in 2:N-1
+    gradients[j] = (L[j-1] * prime(M[j],"Link") * R[j+1])/(localnorm[j]*Z)
+  end
+  gradients[N] = (L[N-1] * prime(M[N],"Link"))/(localnorm[N]*Z)
+  return 2*gradients,log(Z)
+end
 
 """
-Negative log likelihood (multiple bases)
+Negative log likelihood for MPS
 """
 function nll(psi::MPS,data::Array,bases::Array)
   N = length(psi)
@@ -96,46 +132,9 @@ function nll(psi::MPS,data::Array,bases::Array)
   return loss
 end
 
-"""
-Gradients of logZ 
-"""
-function gradlogZ(M::Union{MPS,MPO};localnorm=nothing)
-  N = length(M)
-  L = Vector{ITensor}(undef, N-1)
-  R = Vector{ITensor}(undef, N)
-  
-  if isnothing(localnorm)
-    localnorm = ones(N)
-  end
-  
-  # Sweep right to get L
-  L[1] = dag(M[1]) * prime(M[1],"Link")
-  for j in 2:N-1
-    L[j] = L[j-1] * dag(M[j])
-    L[j] = L[j] * prime(M[j],"Link")
-  end
-  Z = L[N-1] * dag(M[N])
-  Z = real((Z * prime(M[N],"Link"))[])
-
-  # Sweep left to get R
-  R[N] = dag(M[N]) * prime(M[N],"Link")
-  for j in reverse(2:N-1)
-    R[j] = R[j+1] * dag(M[j])
-    R[j] = R[j] * prime(M[j],"Link")
-  end
-  # Get the gradients of the normalization
-  gradients = Vector{ITensor}(undef, N)
-  gradients[1] = prime(M[1],"Link") * R[2]/(localnorm[1]*Z)
-  for j in 2:N-1
-    gradients[j] = (L[j-1] * prime(M[j],"Link") * R[j+1])/(localnorm[j]*Z)
-  end
-  gradients[N] = (L[N-1] * prime(M[N],"Link"))/(localnorm[N]*Z)
-  return 2*gradients,log(Z)
-end
-
 
 """
-Gradients of NLL with local norms (computational basis)
+Gradients of NLL for MPS 
 """
 function gradnll(psi::MPS,data::Array,bases::Array;localnorm=nothing)
   loss = 0.0
@@ -234,14 +233,17 @@ end
 """
 Compute the total gradients
 """
-function gradients(psi::MPS,data::Array,bases::Array;localnorm=nothing)
-  g_logZ,logZ = gradlogZ(psi,localnorm=localnorm)
-  g_nll, nll  = gradnll(psi,data,bases,localnorm=localnorm)
+function gradients(M::Union{MPS,MPO},data::Array,bases::Array;localnorm=nothing)
+  g_logZ,logZ = gradlogZ(M,localnorm=localnorm)
+  g_nll, nll  = gradnll(M,data,bases,localnorm=localnorm)
   grads = g_logZ + g_nll
   loss = logZ + nll
   return grads,loss
 end
 
+"""
+Run QST
+"""
 function statetomography!(psi::MPS,opt::Optimizer;
                           samples::Array,
                           bases::Array,
@@ -323,33 +325,172 @@ function initializeQST(N::Int,χ::Int,ξ::Int;d::Int=2,seed::Int=1234,σ::Float6
   return lpdo
 end
 
-#"""
-#Normalize the LPDO locally and store the local norms
-#"""
-#function lognormalize!(lpdo::MPO)
-#  localnorms = []
-#  blob = dag(lpdo[1]) * prime(lpdo[1],"Link")
-#  localZ = norm(blob)
-#  logZ = 0.5*log(localZ)
-#  blob /= sqrt(localZ)
-#  lpdo[1] /= (localZ^0.25)
-#  push!(localnorms,localZ^0.25)
-#  for j in 2:length(lpdo)-1
-#    blob = blob * dag(lpdo[j])
-#    blob = blob * prime(lpdo[j],"Link")
-#    localZ = norm(blob)
-#    logZ += 0.5*log(localZ)
-#    blob /= sqrt(localZ)
-#    lpdo[j] /= (localZ^0.25)
-#    push!(localnorms,localZ^0.25)  
-#  end
-#  blob = blob * dag(lpdo[length(lpdo)]);
-#  blob = blob * prime(lpdo[length(lpdo)],"Link")
-#  localZ = norm(blob)
-#  lpdo[length(lpdo)] /= sqrt(localZ)
-#  push!(localnorms,sqrt(localZ))
-#  logZ += log(real(blob[]))
-#  return logZ,localnorms
-#end
 
+"""
+Negative log likelihood for LPDO
+"""
+function nll(lpdo::MPO,data::Array,bases::Array)
+  N = length(lpdo)
+  loss = 0.0
+  for n in 1:size(data)[1]
+    x = data[n,:]
+    x .+= 1
+    basis = bases[n,:]
+    
+    if (basis[1] == "Z")
+      prob = prime(lpdo[1],"Link") * setelt(firstind(lpdo[1],tags="Site")=>x[1])
+      Tdag = dag(lpdo[1]) * setelt(firstind(lpdo[1],tags="Site")=>x[1])
+      prob = prob * Tdag 
+    else
+      rotation = makegate(lpdo,"m$(basis[1])",1)
+      prob = prime(lpdo[1],"Link") * rotation * prime(setelt(firstind(lpdo[1],tags="Site")=>x[1]))
+      Tdag = dag(lpdo[1]) * dag(rotation) * prime(setelt(firstind(lpdo[1],tags="Site")=>x[1]))
+      prob = prob * Tdag 
+    end
+    for j in 2:N-1
+      if (basis[j] == "Z")
+        prob = prob * prime(lpdo[j],"Link") * setelt(firstind(lpdo[j],tags="Site")=>x[j])
+        Tdag = dag(lpdo[j]) * setelt(firstind(lpdo[j],tags="Site")=>x[j])
+        prob = prob * Tdag
+      else
+        rotation = makegate(lpdo,"m$(basis[j])",j)
+        prob = prob * prime(lpdo[j],"Link") * rotation * prime(setelt(firstind(lpdo[j],tags="Site")=>x[j]))
+        Tdag = dag(lpdo[j]) * dag(rotation) * prime(setelt(firstind(lpdo[j],tags="Site")=>x[j]))
+        prob = prob * Tdag
+      end
+    end
+    if (basis[N] == "Z")
+      prob = prob * prime(lpdo[N],"Link") * setelt(firstind(lpdo[N],tags="Site")=>x[N])
+      Tdag = dag(lpdo[N]) * setelt(firstind(lpdo[N],tags="Site")=>x[N])
+      prob = prob * Tdag
+    else
+      rotation = makegate(lpdo,"m$(basis[N])",N)
+      prob = prob * prime(lpdo[N],"Link") * rotation * prime(setelt(firstind(lpdo[N],tags="Site")=>x[N]))
+      Tdag = dag(lpdo[N]) * dag(rotation) * prime(setelt(firstind(lpdo[N],tags="Site")=>x[N]))
+      prob = prob * Tdag
+    end
+    loss -= log(real(prob[]))/size(data)[1]
+  end
+  return loss
+end
+
+
+
+"""
+Gradients of NLL for MPS 
+"""
+function gradnll(lpdo::MPO,data::Array,bases::Array;localnorm=nothing)
+  loss = 0.0
+
+  N = length(lpdo)
+  L = Vector{ITensor}(undef, N-1)
+  R = Vector{ITensor}(undef, N)
+  
+  if isnothing(localnorm)
+    localnorm = ones(N)
+  end
+  
+  gradients = ITensor[]
+  for j in 1:N
+    push!(gradients,ITensor(inds(lpdo[j])))
+  end
+  for n in 1:size(data)[1]
+    x = data[n,:] 
+    x.+=1
+    basis = bases[n,:]
+    
+    """ LEFT ENVIRONMENTS """
+    if (basis[1] == "Z")
+      L[1] = prime(lpdo[1],"Link") * setelt(firstind(lpdo[1],tags="Site")=>x[1])
+      Tdag = dag(lpdo[1]) * setelt(firstind(lpdo[1],tags="Site")=>x[1])
+      L[1] = L[1] * Tdag 
+    else
+      rotation = makegate(lpdo,"m$(basis[1])",1)
+      L[1] = prime(lpdo[1],"Link") * rotation * prime(setelt(firstind(lpdo[1],tags="Site")=>x[1]))
+      Tdag = dag(lpdo[1]) * dag(rotation) * prime(setelt(firstind(lpdo[1],tags="Site")=>x[1]))
+      L[1] = L[1] * Tdag 
+    end
+    for j in 2:N-1
+      if (basis[j] == "Z")
+        L[j] = L[j-1] * prime(lpdo[j],"Link") * setelt(firstind(lpdo[j],tags="Site")=>x[j])
+        Tdag = dag(lpdo[j]) * setelt(firstind(lpdo[j],tags="Site")=>x[j])
+        L[j] = L[j] * Tdag
+      else
+        rotation = makegate(lpdo,"m$(basis[j])",j)
+        L[j] = L[j-1] * prime(lpdo[j],"Link") * rotation * prime(setelt(firstind(lpdo[j],tags="Site")=>x[j]))
+        Tdag = dag(lpdo[j]) * dag(rotation) * prime(setelt(firstind(lpdo[j],tags="Site")=>x[j]))
+        L[j] = L[j] * Tdag
+      end
+    end
+    if (basis[N] == "Z")
+      prob = L[N-1] * prime(lpdo[N],"Link") * setelt(firstind(lpdo[N],tags="Site")=>x[N])
+      Tdag = dag(lpdo[N]) * setelt(firstind(lpdo[N],tags="Site")=>x[N])
+      prob = prob * Tdag
+    else
+      rotation = makegate(lpdo,"m$(basis[N])",N)
+      prob = L[N-1] * prime(lpdo[N],"Link") * rotation * prime(setelt(firstind(lpdo[N],tags="Site")=>x[N]))
+      Tdag = dag(lpdo[N]) * dag(rotation) * prime(setelt(firstind(lpdo[N],tags="Site")=>x[N]))
+      prob = prob * Tdag
+    end
+    prob = real(prob[])
+    loss -= log(prob)/size(data)[1]
+    
+    """ RIGHT ENVIRONMENTS """
+    if (basis[N] == "Z")
+      R[N] = prime(lpdo[N],"Link") * setelt(firstind(lpdo[N],tags="Site")=>x[N])
+      Tdag = dag(lpdo[N]) * setelt(firstind(lpdo[N],tags="Site")=>x[N])
+      R[N] = R[N] * Tdag
+    else
+      rotation = makegate(lpdo,"m$(basis[N])",N)
+      R[N] = prime(lpdo[N],"Link") * rotation * prime(setelt(firstind(lpdo[N],tags="Site")=>x[N]))
+      Tdag = dag(lpdo[N]) * dag(rotation) * prime(setelt(firstind(lpdo[N],tags="Site")=>x[N]))
+      R[N] = R[N] * Tdag
+    end
+    for j in reverse(2:N-1)
+      if (basis[j] == "Z")
+        R[j] = R[j+1] * prime(lpdo[j],"Link") * setelt(firstind(lpdo[j],tags="Site")=>x[j])
+        Tdag = dag(lpdo[j]) * setelt(firstind(lpdo[j],tags="Site")=>x[j])
+        R[j] = R[j] * Tdag
+      else
+        rotation = makegate(lpdo,"m$(basis[j])",j)
+        R[j] = R[j+1] * prime(lpdo[j],"Link") * rotation * prime(setelt(firstind(lpdo[j],tags="Site")=>x[j]))
+        Tdag = dag(lpdo[j]) * dag(rotation) * prime(setelt(firstind(lpdo[j],tags="Site")=>x[j]))
+        R[j] = R[j] * Tdag
+      end
+    end
+
+    """ GRADIENTS """
+    if (basis[1] == "Z")
+      Tup = prime(lpdo[1],"Link") * setelt(firstind(lpdo[1],tags="Site")=>x[1])
+      gradients[1] += (Tup * R[2] * setelt(firstind(lpdo[1],tags="Site")=>x[1]))/(localnorm[1]*prob)
+    else
+      rotation = makegate(lpdo,"m$(basis[1])",1)
+      Tup = prime(lpdo[1],"Link") * rotation * prime(setelt(firstind(lpdo[1],tags="Site")=>x[1]))
+      Tdown = dag(rotation) * prime(setelt(firstind(lpdo[1],tags="Site")=>x[1]))
+      gradients[1] += (Tup * R[2] * Tdown)/(localnorm[1]*prob)
+    end
+    for j in 2:N-1
+      if (basis[j] == "Z")
+        Tup = prime(lpdo[j],"Link") * setelt(firstind(lpdo[j],tags="Site")=>x[j])
+        gradients[1j] += (L[j-1] * Tup * R[j+1] * setelt(firstind(lpdo[j],tags="Site")=>x[j]))/(localnorm[j]*prob)
+      else
+        rotation = makegate(lpdo,"m$(basis[j])",j)
+        Tup = prime(lpdo[j],"Link") * rotation * prime(setelt(firstind(lpdo[j],tags="Site")=>x[j]))
+        Tdown = dag(rotation) * prime(setelt(firstind(lpdo[j],tags="Site")=>x[j]))
+        gradients[j] += (L[j-1] * Tup * R[j+1] * Tdown)/(localnorm[j]*prob)
+      end
+    end
+    if (basis[N] == "Z")
+      Tup = prime(lpdo[N],"Link") * setelt(firstind(lpdo[N],tags="Site")=>x[N])
+      gradients[N] += (Tup * L[N-1] * setelt(firstind(lpdo[N],tags="Site")=>x[N]))/(localnorm[N]*prob)
+    else
+      rotation = makegate(lpdo,"m$(basis[N])",N)
+      Tup = prime(lpdo[N],"Link") * rotation * prime(setelt(firstind(lpdo[N],tags="Site")=>x[N]))
+      Tdown = dag(rotation) * prime(setelt(firstind(lpdo[N],tags="Site")=>x[N]))
+      gradients[N] += (Tup * L[N-1] * Tdown)/(localnorm[N]*prob)
+    end
+  end
+  gradients = -2.0*gradients/size(data)[1]
+  return gradients,loss 
+end
 
--- test/qst.jl
+++ test/qst.jl
@@ -83,54 +83,54 @@ function numgradslogZ(M::Union{MPS,MPO};accuracy=1e-8)
   return grad_r-grad_i
 end
 
-function numgradsnll(psi::MPS,data::Array;accuracy=1e-8)
-  N = length(psi)
+function numgradsnll(M::Union{MPS,MPO},data::Array,bases::Array;accuracy=1e-8)
+  N = length(M)
   grad_r = []
   grad_i = []
   for j in 1:N
-    push!(grad_r,zeros(ComplexF64,size(psi[j])))
-    push!(grad_i,zeros(ComplexF64,size(psi[j])))
+    push!(grad_r,zeros(ComplexF64,size(M[j])))
+    push!(grad_i,zeros(ComplexF64,size(M[j])))
   end
   
-  epsilon = zeros(ComplexF64,size(psi[1]));
+  epsilon = zeros(ComplexF64,size(M[1]));
   # Site 1
   for i in 1:length(epsilon)
     epsilon[i] = accuracy
-    eps = ITensor(epsilon,inds(psi[1]))
-    psi[1] += eps
-    loss_p = nll(psi,data) 
-    psi[1] -= eps
-    loss_m = nll(psi,data) 
+    eps = ITensor(epsilon,inds(M[1]))
+    M[1] += eps
+    loss_p = nll(M,data,bases) 
+    M[1] -= eps
+    loss_m = nll(M,data,bases) 
     grad_r[1][i] = (loss_p-loss_m)/(accuracy)
     
     epsilon[i] = im*accuracy
-    eps = ITensor(epsilon,inds(psi[1]))
-    psi[1] += eps
-    loss_p = nll(psi,data) 
-    psi[1] -= eps
-    loss_m = nll(psi,data) 
+    eps = ITensor(epsilon,inds(M[1]))
+    M[1] += eps
+    loss_p = nll(M,data,bases) 
+    M[1] -= eps
+    loss_m = nll(M,data,bases) 
     grad_i[1][i] = (loss_p-loss_m)/(im*accuracy)
     
     epsilon[i] = 0.0
   end
 
   for j in 2:N-1
-    epsilon = zeros(ComplexF64,size(psi[j]));
+    epsilon = zeros(ComplexF64,size(M[j]));
     for i in 1:length(epsilon)
       epsilon[i] = accuracy
-      eps = ITensor(epsilon,inds(psi[j]))
-      psi[j] += eps
-      loss_p = nll(psi,data) 
-      psi[j] -= eps
-      loss_m = nll(psi,data) 
+      eps = ITensor(epsilon,inds(M[j]))
+      M[j] += eps
+      loss_p = nll(M,data,bases) 
+      M[j] -= eps
+      loss_m = nll(M,data,bases) 
       grad_r[j][i] = (loss_p-loss_m)/(accuracy)
       
       epsilon[i] = im*accuracy
-      eps = ITensor(epsilon,inds(psi[j]))
-      psi[j] += eps
-      loss_p = nll(psi,data) 
-      psi[j] -= eps
-      loss_m = nll(psi,data) 
+      eps = ITensor(epsilon,inds(M[j]))
+      M[j] += eps
+      loss_p = nll(M,data,bases) 
+      M[j] -= eps
+      loss_m = nll(M,data,bases) 
       grad_i[j][i] = (loss_p-loss_m)/(im*accuracy)
 
       epsilon[i] = 0.0
@@ -138,101 +138,22 @@ function numgradsnll(psi::MPS,data::Array;accuracy=1e-8)
  end
 
   # Site N
-  epsilon = zeros(ComplexF64,size(psi[N]));
+  epsilon = zeros(ComplexF64,size(M[N]));
   for i in 1:length(epsilon)
     epsilon[i] = accuracy
-    eps = ITensor(epsilon,inds(psi[N]))
-    psi[N] += eps
-    loss_p = nll(psi,data) 
-    psi[N] -= eps
-    loss_m = nll(psi,data) 
+    eps = ITensor(epsilon,inds(M[N]))
+    M[N] += eps
+    loss_p = nll(M,data,bases) 
+    M[N] -= eps
+    loss_m = nll(M,data,bases) 
     grad_r[N][i] = (loss_p-loss_m)/(accuracy)
 
     epsilon[i] = im*accuracy
-    eps = ITensor(epsilon,inds(psi[N]))
-    psi[N] += eps
-    loss_p = nll(psi,data) 
-    psi[N] -= eps
-    loss_m = nll(psi,data) 
-    grad_i[N][i] = (loss_p-loss_m)/(im*accuracy)
-    
-    epsilon[i] = 0.0
-  end
-
-  return grad_r-grad_i
-end
-
-function numgradsnll(psi::MPS,data::Array,bases::Array;accuracy=1e-8)
-  N = length(psi)
-  grad_r = []
-  grad_i = []
-  for j in 1:N
-    push!(grad_r,zeros(ComplexF64,size(psi[j])))
-    push!(grad_i,zeros(ComplexF64,size(psi[j])))
-  end
-  
-  epsilon = zeros(ComplexF64,size(psi[1]));
-  # Site 1
-  for i in 1:length(epsilon)
-    epsilon[i] = accuracy
-    eps = ITensor(epsilon,inds(psi[1]))
-    psi[1] += eps
-    loss_p = nll(psi,data,bases) 
-    psi[1] -= eps
-    loss_m = nll(psi,data,bases) 
-    grad_r[1][i] = (loss_p-loss_m)/(accuracy)
-    
-    epsilon[i] = im*accuracy
-    eps = ITensor(epsilon,inds(psi[1]))
-    psi[1] += eps
-    loss_p = nll(psi,data,bases) 
-    psi[1] -= eps
-    loss_m = nll(psi,data,bases) 
-    grad_i[1][i] = (loss_p-loss_m)/(im*accuracy)
-    
-    epsilon[i] = 0.0
-  end
-
-  for j in 2:N-1
-    epsilon = zeros(ComplexF64,size(psi[j]));
-    for i in 1:length(epsilon)
-      epsilon[i] = accuracy
-      eps = ITensor(epsilon,inds(psi[j]))
-      psi[j] += eps
-      loss_p = nll(psi,data,bases) 
-      psi[j] -= eps
-      loss_m = nll(psi,data,bases) 
-      grad_r[j][i] = (loss_p-loss_m)/(accuracy)
-      
-      epsilon[i] = im*accuracy
-      eps = ITensor(epsilon,inds(psi[j]))
-      psi[j] += eps
-      loss_p = nll(psi,data,bases) 
-      psi[j] -= eps
-      loss_m = nll(psi,data,bases) 
-      grad_i[j][i] = (loss_p-loss_m)/(im*accuracy)
-
-      epsilon[i] = 0.0
-    end
- end
-
-  # Site N
-  epsilon = zeros(ComplexF64,size(psi[N]));
-  for i in 1:length(epsilon)
-    epsilon[i] = accuracy
-    eps = ITensor(epsilon,inds(psi[N]))
-    psi[N] += eps
-    loss_p = nll(psi,data,bases) 
-    psi[N] -= eps
-    loss_m = nll(psi,data,bases) 
-    grad_r[N][i] = (loss_p-loss_m)/(accuracy)
-
-    epsilon[i] = im*accuracy
-    eps = ITensor(epsilon,inds(psi[N]))
-    psi[N] += eps
-    loss_p = nll(psi,data,bases) 
-    psi[N] -= eps
-    loss_m = nll(psi,data,bases) 
+    eps = ITensor(epsilon,inds(M[N]))
+    M[N] += eps
+    loss_p = nll(M,data,bases) 
+    M[N] -= eps
+    loss_m = nll(M,data,bases) 
     grad_i[N][i] = (loss_p-loss_m)/(im*accuracy)
     
     epsilon[i] = 0.0
@@ -244,115 +165,115 @@ end
 
 
 
-#""" MPS TOMOGRAPHY TESTS """
-#
-#@testset "mps-qst: lognormalization" begin
-#  N = 10
-#  χ = 4
-#  psi = initializeQST(N,χ)
-#  @test length(psi) == N
-#  logZ1 = 2.0*lognorm(psi)
-#  logZ2,_ = lognormalize!(psi)
-#  @test logZ1 ≈ logZ2
-#  psi = initializeQST(N,χ)
-#  lognormalize!(psi)
-#  @test norm(psi) ≈ 1
-#end
-#
-#@testset "mps-qst: grad logZ" begin
-#  N = 5
-#  χ = 4
-#  psi = initializeQST(N,χ)
-#  alg_grad,_ = gradlogZ(psi)
-#  num_grad = numgradslogZ(psi)
-#  for j in 1:N
-#    @test array(alg_grad[j]) ≈ num_grad[j] rtol=1e-3
-#  end
-#  psi = initializeQST(N,χ)
-#  logZ,localnorms = lognormalize!(psi)
-#  @test norm(psi)^2 ≈ 1
-#  alg_grad_localnorm,_ = gradlogZ(psi,localnorm=localnorms)
-#  for j in 1:N
-#    @test array(alg_grad[j]) ≈ array(alg_grad_localnorm[j]) rtol=1e-3
-#  end
-#end
-#
-#@testset "mps-qst: grad nll" begin
-#  N = 5
-#  χ = 4
-#  nsamples = 100
-#  Random.seed!(1234)
-#  data = rand(0:1,nsamples,N)
-#  bases = generatemeasurementsettings(N,nsamples,bases_id=["X","Y","Z"]) 
-#  psi = initializeQST(N,χ)
-#  num_grad = numgradsnll(psi,data,bases)
-#  alg_grad,loss = gradnll(psi,data,bases)
-#  for j in 1:N
-#    @test array(alg_grad[j]) ≈ num_grad[j] rtol=1e-3
-#  end
-#  psi = initializeQST(N,χ)
-#  logZ,localnorms = lognormalize!(psi)
-#  @test norm(psi)^2 ≈ 1
-#  alg_grad_localnorm,loss = gradnll(psi,data,bases,localnorm=localnorms)
-#  for j in 1:N
-#    @test array(alg_grad[j]) ≈ array(alg_grad_localnorm[j]) rtol=1e-3
-#  end
-#end
-#
-#
-#@testset "mps-qst: full gradients" begin
-#  N = 5
-#  χ = 4
-#  nsamples = 100
-#  Random.seed!(1234)
-#  data = rand(0:1,nsamples,N)
-#  bases = generatemeasurementsettings(N,nsamples,bases_id=["X","Y","Z"])
-#
-#  psi = initializeQST(N,χ)
-#  logZ = 2.0*log(norm(psi))
-#  NLL  = nll(psi,data,bases)
-#  ex_loss = logZ + NLL
-#  num_gradZ = numgradslogZ(psi)
-#  num_gradNLL = numgradsnll(psi,data,bases)
-#  num_grads = num_gradZ + num_gradNLL
-#
-#  alg_grads,loss = gradients(psi,data,bases)
-#  @test ex_loss ≈ loss
-#  for j in 1:N
-#    @test array(alg_grads[j]) ≈ num_grads[j] rtol=1e-3
-#  end
-#
-#  psi = initializeQST(N,χ)
-#  logZ,localnorms = lognormalize!(psi)
-#  NLL  = nll(psi,data,bases)
-#  ex_loss = NLL
-#  @test norm(psi)^2 ≈ 1
-#  
-#  alg_grads,loss = gradients(psi,data,bases,localnorm=localnorms)
-#  @test ex_loss ≈ loss
-#  for j in 1:N
-#    @test array(alg_grads[j]) ≈ num_grads[j] rtol=1e-3
-#  end
-#end
-#
+""" MPS TOMOGRAPHY TESTS """
+
+@testset "mps-qst: lognormalization" begin
+  N = 10
+  χ = 4
+  psi = initializeQST(N,χ)
+  @test length(psi) == N
+  logZ1 = 2.0*lognorm(psi)
+  logZ2,_ = lognormalize!(psi)
+  @test logZ1 ≈ logZ2
+  psi = initializeQST(N,χ)
+  lognormalize!(psi)
+  @test norm(psi) ≈ 1
+end
+
+@testset "mps-qst: grad logZ" begin
+  N = 5
+  χ = 4
+  psi = initializeQST(N,χ)
+  alg_grad,_ = gradlogZ(psi)
+  num_grad = numgradslogZ(psi)
+  for j in 1:N
+    @test array(alg_grad[j]) ≈ num_grad[j] rtol=1e-3
+  end
+  psi = initializeQST(N,χ)
+  logZ,localnorms = lognormalize!(psi)
+  @test norm(psi)^2 ≈ 1
+  alg_grad_localnorm,_ = gradlogZ(psi,localnorm=localnorms)
+  for j in 1:N
+    @test array(alg_grad[j]) ≈ array(alg_grad_localnorm[j]) rtol=1e-3
+  end
+end
+
+@testset "mps-qst: grad nll" begin
+  N = 5
+  χ = 4
+  nsamples = 100
+  Random.seed!(1234)
+  data = rand(0:1,nsamples,N)
+  bases = generatemeasurementsettings(N,nsamples,bases_id=["X","Y","Z"]) 
+  psi = initializeQST(N,χ)
+  num_grad = numgradsnll(psi,data,bases)
+  alg_grad,loss = gradnll(psi,data,bases)
+  for j in 1:N
+    @test array(alg_grad[j]) ≈ num_grad[j] rtol=1e-3
+  end
+  psi = initializeQST(N,χ)
+  logZ,localnorms = lognormalize!(psi)
+  @test norm(psi)^2 ≈ 1
+  alg_grad_localnorm,loss = gradnll(psi,data,bases,localnorm=localnorms)
+  for j in 1:N
+    @test array(alg_grad[j]) ≈ array(alg_grad_localnorm[j]) rtol=1e-3
+  end
+end
+
+
+@testset "mps-qst: full gradients" begin
+  N = 5
+  χ = 4
+  nsamples = 100
+  Random.seed!(1234)
+  data = rand(0:1,nsamples,N)
+  bases = generatemeasurementsettings(N,nsamples,bases_id=["X","Y","Z"])
+
+  psi = initializeQST(N,χ)
+  logZ = 2.0*log(norm(psi))
+  NLL  = nll(psi,data,bases)
+  ex_loss = logZ + NLL
+  num_gradZ = numgradslogZ(psi)
+  num_gradNLL = numgradsnll(psi,data,bases)
+  num_grads = num_gradZ + num_gradNLL
+
+  alg_grads,loss = gradients(psi,data,bases)
+  @test ex_loss ≈ loss
+  for j in 1:N
+    @test array(alg_grads[j]) ≈ num_grads[j] rtol=1e-3
+  end
+
+  psi = initializeQST(N,χ)
+  logZ,localnorms = lognormalize!(psi)
+  NLL  = nll(psi,data,bases)
+  ex_loss = NLL
+  @test norm(psi)^2 ≈ 1
+  
+  alg_grads,loss = gradients(psi,data,bases,localnorm=localnorms)
+  @test ex_loss ≈ loss
+  for j in 1:N
+    @test array(alg_grads[j]) ≈ num_grads[j] rtol=1e-3
+  end
+end
+
 
 """ LPDO TOMOGRAPHY TESTS """
-#
-#@testset "lpdo-qst: lognormalization" begin
-#  N = 10
-#  χ = 4
-#  ξ = 2
-#  lpdo = initializeQST(N,χ,ξ)
-#  @test length(lpdo) == N
-#  logZ1 = 2.0*lognorm(lpdo)
-#  logZ2,_ = lognormalize!(lpdo)
-#  @test logZ1 ≈ logZ2
-#  lpdo = initializeQST(N,χ,ξ)
-#  lognormalize!(lpdo)
-#  @test norm(lpdo) ≈ 1
-#end
 
-@testset "lpdo-qst: complex grad logZ" begin
+@testset "lpdo-qst: lognormalization" begin
+  N = 10
+  χ = 4
+  ξ = 2
+  lpdo = initializeQST(N,χ,ξ)
+  @test length(lpdo) == N
+  logZ1 = 2.0*lognorm(lpdo)
+  logZ2,_ = lognormalize!(lpdo)
+  @test logZ1 ≈ logZ2
+  lpdo = initializeQST(N,χ,ξ)
+  lognormalize!(lpdo)
+  @test norm(lpdo) ≈ 1
+end
+
+@testset "lpdo-qst: grad logZ" begin
   N = 5
   χ = 4
   ξ = 3
@@ -382,6 +303,32 @@ end
   end
   alg_gradient = permutedims(array(alg_grad[N]),[2,1,3])
   @test alg_gradient ≈ num_grad[N] rtol=1e-3
+end
+
+@testset "lpdo-qst: grad nll" begin
+  N = 5
+  χ = 4
+  ξ = 3
+  nsamples = 100
+  Random.seed!(1234)
+  data = rand(0:1,nsamples,N)
+  bases = generatemeasurementsettings(N,nsamples,bases_id=["X","Y","Z"])
+  lpdo = initializeQST(N,χ,ξ)
+  num_grad = numgradsnll(lpdo,data,bases)
+  alg_grad,loss = gradnll(lpdo,data,bases)
+  ex_loss = nll(lpdo,data,bases)
+  @test ex_loss == loss
+  for j in 1:N
+    @test array(alg_grad[j]) ≈ num_grad[j] rtol=1e-3
+  end
+
+  lpdo = initializeQST(N,χ,ξ)
+  logZ,localnorms = lognormalize!(lpdo)
+  @test norm(lpdo)^2 ≈ 1
+  alg_grad,loss = gradnll(lpdo,data,bases,localnorm=localnorms)
+  for j in 1:N
+    @test array(alg_grad[j]) ≈ num_grad[j] rtol=1e-3
+  end
 end
 
 
