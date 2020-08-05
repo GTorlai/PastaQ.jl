01bfef71f08683735e4d237faee3862e14736bc0
gtorlai <torlaigiacomo@gmail.com>
2020-08-05 14:12:13 -0400

    Update interface in applygate

--- src/quantumcircuit/circuitops.jl
+++ src/quantumcircuit/circuitops.jl
@@ -30,119 +30,17 @@ end
 makegate(M::Union{MPS,MPO}, gatename::String, sites::Union{Int, Tuple}, params::NamedTuple) =
   makegate(M, gatename, sites; params...)
 
-function applygate!(M::MPS,
-                   gatename::String,
-                   site::Int;
-                   cutoff = 1e-10,
-                   kwargs...)
-  gate = makegate(M,gatename,site; kwargs...)
-  M[site] = gate * M[site]
-  noprime!(M[site])
-end
-
-function applygate!(M::MPO,
-                   gatename::String,
-                   site::Int;
-                   cutoff = 1e-10,
-                   kwargs...)
-  gate = makegate(M,gatename,site; kwargs...)
-  M[site] = gate * prime(M[site],tags="Site",plev=1)
-  prime!(M[site],tags="Site",-1)
-end
-
-# Apply 1Q gate using a pre-generated gate tensor
-function applygate!(M::MPS,gate::ITensor{2}; kwargs...)
-  site = getsitenumber(firstind(gate,"Site")) 
-  M[site] = gate * M[site]
-  noprime!(M[site])
-end
-
-# Apply 1Q gate using a pre-generated gate tensor
-function applygate!(M::MPO,gate::ITensor{2}; kwargs...)
-  site = getsitenumber(firstind(gate; tags="Site", plev=0)) 
-  M[site] = gate * prime(M[site],tags="Site",plev=1)
-  prime!(M[site],tags="Site",-1)
-end
-
-function swap!(M::MPS,site1::Int,site2::Int,cutoff::Float64)
-  nswaps = abs(site1-site2)-1
-  if site1 > site2
-    start = site2
-  else
-    start = site1
-  end
-  for n in 1:nswaps
-    replacebond!(M,start+n-1,M[start+n-1]*M[start+n],swapsites=true,cutoff=cutoff)
-  end
-  newsite1 = start+nswaps
-  newsite2 = newsite1 + 1
-  return M,newsite1,newsite2 
-end
-
-function unswap!(M::MPS,site1::Int,site2::Int,cutoff::Float64)
-  nswaps = abs(site1-site2)-1
-  if site1 > site2
-    start = site2
-  else
-    start = site1
-  end
-  for n in 1:nswaps
-    replacebond!(M,start+nswaps-n,M[start+nswaps-n+1]*M[start+nswaps-n],swapsites=true,cutoff=cutoff)
-  end
+function applygate!(M::Union{MPS,MPO},gatename::String,sites::Union{Int,Tuple};kwargs...)
+  g = makegate(M,gatename,sites;kwargs...)
+  Mc = apply(g,M;kwargs...)
+  M[:] = Mc
   return M
 end
 
-# Apply 2Q gate using a pre-generated gate tensor
-function applygate!(M::MPS, gate::ITensor{4}; cutoff = 1e-10)
-  s1 = getsitenumber(inds(gate,plev=1)[1]) 
-  s2 = getsitenumber(inds(gate,plev=1)[2]) 
-  
-  if abs(s1-s2) != 1
-    M,site1,site2 = swap!(M,s1,s2,cutoff)
-  else
-    site1=s1
-    site2=s2
-  end
-  orthogonalize!(M,site1)
-  blob = M[site1] * M[site2]
-  blob = gate * blob
-  noprime!(blob)
-  U,S,V = svd(blob,inds(M[site1]),cutoff=cutoff)
-  M[site1] = U
-  M[site2] = S*V
-  
-  if abs(s1-s2) != 1
-    M = unswap!(M,s1,s2,cutoff)
-  end
-end
-
-# Apply 2Q gate using a pre-generated gate tensor
-function applygate!(M::MPO, gate::ITensor{4}; cutoff = 1e-10)
-  s1 = getsitenumber(inds(gate,plev=1)[1]) 
-  s2 = getsitenumber(inds(gate,plev=1)[2]) 
-  
-  @assert(abs(s1-s2)==1)
-  #TODO use swaps to handle long-range gates
- 
-  orthogonalize!(M,s1)
-  blob = M[s1] * M[s2]
-  blob = gate * prime(blob,tags="Site",plev=1)
-  prime!(blob,tags="Site",-1)
-  
-  U,S,V = svd(blob,inds(M[s1]),cutoff=cutoff)
-  M[s1] = U
-  M[s2] = S*V
-end
-
-# Apply 2Q gate in the form ("CX", (1,2))
-function applygate!(M::Union{MPS,MPO},
-                   gatename::String,
-                   site::Tuple;
-                   cutoff = 1e-10,
-                   kwargs...)
-  # Construct the gate tensor
-  gate = makegate(M,gatename,site; kwargs...)
-  applygate!(M,gate,cutoff=cutoff) 
+function applygate!(M::Union{MPS,MPO},gate_tensor::ITensor; kwargs...)
+  Mc = apply(gate_tensor,M;kwargs...)
+  M[:] = Mc
+  return M
 end
 
 # Retrieve the qubit number from a site index
