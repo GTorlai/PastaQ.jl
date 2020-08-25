3c75bbefaaf595280731061ff225401e389917f1
gtorlai <torlaigiacomo@gmail.com>
2020-08-25 10:29:52 -0400

    2d circuits with cycles

--- src/quantumcircuit/circuits.jl
+++ src/quantumcircuit/circuits.jl
@@ -1,4 +1,67 @@
 """
+Circuit geometries
+"""
+# Returns two-qubit bonds for a linear array
+function lineararray(N::Int)
+  twoqubit_bonds = []
+  # Cycle 1
+  cycle = []
+  for j in 1:2:N-1
+    push!(cycle,(j,j+1))
+  end
+  push!(twoqubit_bonds,cycle)
+  # Cycle 2
+  cycle = []
+  for j in 2:2:N-1
+    push!(cycle,(j,j+1))
+  end
+  push!(twoqubit_bonds,cycle)
+  return twoqubit_bonds
+end
+
+# Get site number from coordinate
+coord_to_site(Lx::Int,Ly::Int,x::Int,y::Int) = Lx*(y-1) + x
+
+# Returns two-qubit bonds fro a square array
+function squarearray(Lx::Int,Ly::Int)
+  N = Lx * Ly
+  twoqubit_bonds = []
+  # Cycle 1
+  cycle = []
+  for y in 1:2:Ly-1
+    for x in 1:Lx
+      push!(cycle,(coord_to_site(Lx,Ly,x,y),coord_to_site(Lx,Ly,x,y+1)))
+    end
+  end
+  push!(twoqubit_bonds,cycle)
+  # Cycle 2
+  cycle = []
+  for y in 1:2:Ly-1
+    for x in 2:Lx
+      push!(cycle,(coord_to_site(Lx,Ly,x,y),coord_to_site(Lx,Ly,x-1,y+1)))
+    end
+  end
+  push!(twoqubit_bonds,cycle)
+  # Cycle 3
+  cycle = []
+  for y in 2:2:Ly-1
+    for x in 1:Lx-1
+      push!(cycle,(coord_to_site(Lx,Ly,x,y),coord_to_site(Lx,Ly,x+1,y+1)))
+    end
+  end
+  push!(twoqubit_bonds,cycle)
+  # Cycle 4
+  cycle = []
+  for y in 2:2:Ly-1
+    for x in 1:Lx
+      push!(cycle,(coord_to_site(Lx,Ly,x,y),coord_to_site(Lx,Ly,x,y+1)))
+    end
+  end
+  push!(twoqubit_bonds,cycle)
+  return twoqubit_bonds
+end
+
+"""
 Add a list of gates to gates (data structure) 
 """
 function appendgates!(gates::Vector{<:Tuple},newgates::Vector{<:Tuple})
@@ -50,6 +113,9 @@ function randomrotationlayer!(gates::Array,N::Int)
   return gates
 end
 
+"""
+Layer of two-qubit gates 
+"""
 function twoqubitlayer(gatename::String,bonds::Array)
   gates = Tuple[]
   for bond in bonds
@@ -64,69 +130,13 @@ function twoqubitlayer!(gates::Array,gatename::String,bonds::Array)
   return gates
 end
 
-function lineararray(N::Int)
-  twoqubit_bonds = []
-  # Cycle 1
-  cycle = []
-  for j in 1:2:N-1
-    push!(cycle,(j,j+1))
-  end
-  push!(twoqubit_bonds,cycle)
-  # Cycle 2
-  cycle = []
-  for j in 2:2:N-1
-    push!(cycle,(j,j+1))
-  end
-  push!(twoqubit_bonds,cycle)
-  return twoqubit_bonds
-end
-
-coord_to_site(Lx::Int,Ly::Int,x::Int,y::Int) = Lx*(y-1) + x
-
-function squarearray(Lx::Int,Ly::Int)
-  N = Lx * Ly
-  twoqubit_bonds = []
-  # Cycle 1
-  cycle = []
-  for y in 1:2:Ly-1
-    for x in 1:Lx
-      push!(cycle,(coord_to_site(Lx,Ly,x,y),coord_to_site(Lx,Ly,x,y+1)))
-    end
-  end
-  push!(twoqubit_bonds,cycle)
-  # Cycle 2
-  cycle = []
-  for y in 1:2:Ly-1
-    for x in 2:Lx
-      push!(cycle,(coord_to_site(Lx,Ly,x,y),coord_to_site(Lx,Ly,x-1,y+1)))
-    end
-  end
-  push!(twoqubit_bonds,cycle)
-  # Cycle 3
-  cycle = []
-  for y in 2:2:Ly-1
-    for x in 1:Lx-1
-      push!(cycle,(coord_to_site(Lx,Ly,x,y),coord_to_site(Lx,Ly,x+1,y+1)))
-    end
-  end
-  push!(twoqubit_bonds,cycle)
-  # Cycle 4
-  cycle = []
-  for y in 2:2:Ly-1
-    for x in 1:Lx
-      push!(cycle,(coord_to_site(Lx,Ly,x,y),coord_to_site(Lx,Ly,x,y+1)))
-    end
-  end
-  push!(twoqubit_bonds,cycle)
-  return twoqubit_bonds
-end
-
-function randomquantumcircuit(N::Int,depth::Int;
+"""
+Random quantum circuit
+"""
+function randomquantumcircuit(N::Int,depth::Int,twoqubit_bonds::Array;
                               twoqubitgate   = "CX",
                               onequbitgates  = ["Rn"])
-  twoqubit_bonds = lineararray(N)
   gates = Tuple[]
-  
   numgates_1q = length(onequbitgates)
   
   for d in 1:depth
@@ -143,5 +153,24 @@ function randomquantumcircuit(N::Int,depth::Int;
     end
   end
   return gates
+end
+
+function randomquantumcircuit(N::Int,depth::Int;
+                              twoqubitgate   = "CX",
+                              onequbitgates  = ["Rn"])
+  twoqubit_bonds = lineararray(N)
+  return randomquantumcircuit(N,depth,twoqubit_bonds;
+                              twoqubitgate=twoqubitgate,
+                              onequbitgates=onequbitgates)
+end
+
+function randomquantumcircuit(Lx::Int,Ly::Int,depth::Int;
+                              twoqubitgate   = "CX",
+                              onequbitgates  = ["Rn"])
+  twoqubit_bonds = squarearray(Lx,Ly)
+  N = Lx * Ly
+  return randomquantumcircuit(N,depth,twoqubit_bonds;
+                              twoqubitgate=twoqubitgate,
+                              onequbitgates=onequbitgates)
 end
 
