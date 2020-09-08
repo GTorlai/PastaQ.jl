03fd813705ac6402a5c468f23bd65ed98e793a94
gtorlai <torlaigiacomo@gmail.com>
2020-09-08 12:44:43 -0400

    Data generation for finite-T Ising

--- examples/3_data_ising.jl
+++ examples/3_data_ising.jl
@@ -4,21 +4,38 @@ using Random
 using HDF5
 
 Random.seed!(1234)
-
 N = 100
 h = 1.0
+target = "GS"
 
-ψ = transversefieldising(N,h,iter=20)
+if target == "GS"
+  ψ = transversefieldising(N,h,iter=20)
+  
+  nshots = 50000
+  println("Generating data...")
+  bases = generatemeasurementsettings(N,nshots,bases_id=["X","Y","Z"])
+  data = generatedata(ψ,nshots,bases)
+  
+  output_path = "data_ising_N$(N).h5"
+  h5open(output_path, "w") do file
+    write(file,"data",data)
+    write(file,"psi",ψ)
+  end
 
-nshots = 50000
-println("Generating data...")
-bases = generatemeasurementsettings(N,nshots,bases_id=["X","Y","Z"])
-
-data = generatedata(ψ,nshots,bases)
-
-output_path = "data_ising_N$(N).h5"
-
-h5open(output_path, "w") do file
-  write(file,"data",data)
-  write(file,"psi",ψ)
+elseif target == "thermal"
+  β = 1.0
+  τ = 0.01
+  
+  ρ = transversefieldising(N,h,β=β,τ=τ) 
+  
+  println("Generating data...")
+  bases = generatemeasurementsettings(N,nshots,bases_id=["X","Y","Z"])
+  data = generatedata(ρ,nshots,bases)
+  
+  output_path = "data_ising_N$(N)_beta=1.0.h5"
+  
+  h5open(output_path, "w") do file
+    write(file,"data",data)
+    write(file,"rho",ρ)
+  end
 end
