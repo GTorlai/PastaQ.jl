fc0492bf2cdd485d4f6e655f746d34441d82ae69
gtorlai <torlaigiacomo@gmail.com>
2020-09-24 17:27:29 -0400

    readme

--- examples/3_qst_circuit.jl
+++ examples/3_qst_circuit.jl
@@ -4,16 +4,25 @@ using ITensors
 
 Random.seed!(1234)
 
-# Unitary circuit
+# 1. Quantum state tomography on measurements generated
+# as the output of a unitary quantum circuit
+
+# Load target state and measurement data
 Ψ,data = loaddata("../data/qst_circuit.h5")
 
-N = length(Ψ)
-χ = maxlinkdim(Ψ)
+# Set parameters 
+N = length(Ψ)     # Number of qubits
+χ = maxlinkdim(Ψ) # Bond dimension of variational MPS
 
+# Initialize the variational MPS
 ψ0 = initializetomography(N;χ=χ,σ=0.1)
+
+# Initialize stochastic gradient descent optimizer
 opt = SGD(ψ0;η = 0.005)
 
-println("Training...")
+# Run quantum state tomography, where a variational MPS `|ψ(θ)⟩`
+# is optimized to mimimize the cross entropy between the data and 
+# the tensor-network distribution `P(x) = |⟨x|ψ(θ)⟩|²`.
 ψ = tomography(ψ0,data,opt;
                batchsize=1000,
                epochs=10,
@@ -21,16 +30,28 @@ println("Training...")
                localnorm=true)
 @show ψ
 
-# Noisy circuit
+
+# 2. Quantum state tomography on measurements generated
+# as the output of a noisy quantum circuit
+
+# Load target state and measurement data
 ϱ,data = loaddata("../data/qst_circuit_noisy.h5")
-N = length(ϱ)
-χ = maxlinkdim(ϱ)
-ξ = 2
 
+# Set parameters
+N = length(ϱ)     # Number of qubits
+χ = maxlinkdim(ϱ) # Bond dimension of variational LPDO
+ξ = 2             # Kraus dimension of variational LPDO
 
+# Initialize the LPDO
 ρ0 = initializetomography(N;χ=χ,ξ=ξ,σ=0.1)
+
+# Initialize stochastic gradient descent optimizer
 opt = SGD(ρ0;η = 0.01)
-println("Training...")
+
+# Run quantum state tomography, where a variational LPDO `Γ(θ)`
+# is optimized to mimimize the cross entropy between the data and 
+# the tensor-network distribution `P(x) = ⟨x|ρ(θ)|x⟩`, where
+# `ρ = ΓΓ†` is the corresponding density operator..
 ρ = tomography(ρ0,data,opt;
                batchsize=1000,
                epochs=10,
--- examples/4_qpt_circuit.jl
+++ examples/4_qpt_circuit.jl
@@ -2,17 +2,25 @@ using PastaQ
 using Random
 using ITensors
 
-# Unitary circuit
 Random.seed!(1234)
+
+# 1. Quantum process tomography of a unitary circuit
+
+# Load target state and measurements. Each samples is built out
+# of a input state (`data_in`) to the quantum channel, and the
+# measurement output (`data_out`) after a local basis rotation.
 Φ,data_in,data_out = loaddata("../data/qpt_circuit.h5";process=true)
 
-N = length(Φ)
-χ = maxlinkdim(Φ)
+# Set parameters
+N = length(Φ)     # Number of qubits
+χ = maxlinkdim(Φ) # Bond dimension of variational MPS
 
+# Initialize a variational MPS
 Γ0 = initializetomography(N;χ=χ,σ=0.1)
+
+# Initialize stochastic gradient descent optimizer
 opt = SGD(Γ0;η = 0.1)
 
-println("Training...")
 Γ = tomography(Γ0,data_in,data_out,opt;
                batchsize=500,
                epochs=20,
