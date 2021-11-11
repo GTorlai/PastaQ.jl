64d4f8ddb4bd30effd800955930ef32bc2728445
gtorlai <torlaigiacomo@gmail.com>
2021-11-11 10:10:36 -0800

    resolve gradients of gates

--- examples/vqe/current_bugs.jl
+++ examples/vqe/current_bugs.jl
@@ -2,20 +2,71 @@ using PastaQ
 using ITensors
 using Zygote
 using Random
+import PastaQ: _trottercircuit
+
+function _trotterlayer(H::Vector{<:Tuple}, δτ::Number)
+  layer = Tuple[]
+
+  for Hk in H
+    coupling = Hk[1]
+    localop = Hk[2]
+    support = Hk[3]
+
+    # single-qubit gate
+    if length(support) == 1
+      g = (localop, support[1], (f = x -> exp(-0.5 * δτ * coupling * x),))
+      layer = vcat(layer, [g])
+    # multi-qubit gate
+    else
+      g = (localop, support, (f = x -> exp(-0.5 * δτ * coupling * x),))
+      layer = vcat(layer, [g])
+    end
+  end
+  return vcat(layer, reverse(layer))
+end
+
+function PastaQ._trottercircuit(H::Vector{<:Tuple}, τs::Vector; order::Int = 2, layered::Bool = true, kwargs...)
+  nlayers = length(τs) - 1
+  # XXX Zygote
+  #Hs = repeat([H], nlayers)
+  Hs = [H for _ in 1:nlayers]
+  _trottercircuit(Hs, τs; kwargs...) 
+end
+
+function PastaQ._trottercircuit(H::Vector{<:Vector{<:Tuple}}, τs::Vector; order::Int = 2, layered::Bool = true, kwargs...)
+  @assert length(H) == (length(τs) -1) || length(H) == length(τs)
+  δτs = diff(τs)
+  circuit = [_trotterlayer(H[t], δτs[t]) for t in 1:length(δτs)]
+  layered && return circuit
+  return reduce(vcat, circuit)
+end
+
 
 N = 2
 q = qudits(N; dim = 4)
-
 function variational_circuit(θ⃗)
-  H = OpSum()
-  for n in 1:N
+  H = Tuple[] 
+  for n in 1:N-1
     # is this related to each term being or not hermitian? the Trotter step?
     #H += θ⃗[n], "a† + a", n
-    H += θ⃗[n], "a†", n
-    H += θ⃗[n], "a", n
+    #H += θ⃗[n], "a†", n
+    #H += θ⃗[n], "a", n
+    H = vcat(H, [(θ⃗[n], "a†a", (n,n+1))])
   end
-  return trottercircuit(H; δt=0.1, t=1.0, order = 2, layered = true)
+  return trottercircuit(H; δt=0.1, t=1.0, order = 2, layered = false)
 end
+
+#function variational_circuit(θ⃗)
+#  H = OpSum()
+#  for n in 1:N-1
+#    # is this related to each term being or not hermitian? the Trotter step?
+#    #H += θ⃗[n], "a† + a", n
+#    #H += θ⃗[n], "a†", n
+#    #H += θ⃗[n], "a", n
+#    H += θ⃗[n], "a†a", (n,n+1)
+#  end
+#  return trottercircuit(H; δt=0.1, t=1.0, order = 2, layered = false)
+#end
 
 Random.seed!(1234)
 θ⃗ = rand(N)
@@ -41,45 +92,44 @@ for k in 1:length(θ⃗)
   @show ∇ad[k] - ∇num
 end
 
-
-N = 2
-q = qubits(N)
-
-function variational_circuit(θ⃗)
-  H = OpSum()
-  for n in 1:N
-    # is this related to each term being or not hermitian? the Trotter step?
-    H += θ⃗[n], "X", n
-  end
-  H += θ⃗[1], "CX", 1,2
-  return trottercircuit(H; δt=0.1, t=1.0, order = 2, layered = true)
-end
-
-Random.seed!(1234)
-θ⃗ = rand(N)
-ψ = randomstate(q; normalize = true)
-ϕ = randomstate(q; normalize = true)
-
-function loss(θ⃗)
-  circuit = variational_circuit(θ⃗)
-  U = buildcircuit(ψ, circuit)
-  return -abs2(PastaQ.inner_circuit(ϕ, U, ψ))
-end
-
-∇ad = loss'(θ⃗)
-ϵ = 1e-5
-for k in 1:length(θ⃗)
-  θ⃗[k] += ϵ
-  f₊ = loss(θ⃗)
-  θ⃗[k] -= 2*ϵ
-  f₋ = loss(θ⃗)
-  ∇num = (f₊ - f₋)/(2ϵ)
-  θ⃗[k] += ϵ
-  println("∇ad = ",∇ad[k],"  ∇num = ",∇num)
-  @show ∇ad[k] - ∇num
-end
-
-
-
-
-
+#
+#N = 2
+#q = qubits(N)
+#
+#function variational_circuit(θ⃗)
+#  H = OpSum()
+#  for n in 1:N
+#    H += θ⃗[n], "X", n
+#  end
+#  H += θ⃗[1], "CX", 1,2
+#  return trottercircuit(H; δt=0.1, t=1.0, order = 2, layered = true)
+#end
+#
+#Random.seed!(1234)
+#θ⃗ = rand(N)
+#ψ = randomstate(q; normalize = true)
+#ϕ = randomstate(q; normalize = true)
+#
+#function loss(θ⃗)
+#  circuit = variational_circuit(θ⃗)
+#  U = buildcircuit(ψ, circuit)
+#  return -abs2(PastaQ.inner_circuit(ϕ, U, ψ))
+#end
+#
+#∇ad = loss'(θ⃗)
+#ϵ = 1e-5
+#for k in 1:length(θ⃗)
+#  θ⃗[k] += ϵ
+#  f₊ = loss(θ⃗)
+#  θ⃗[k] -= 2*ϵ
+#  f₋ = loss(θ⃗)
+#  ∇num = (f₊ - f₋)/(2ϵ)
+#  θ⃗[k] += ϵ
+#  println("∇ad = ",∇ad[k],"  ∇num = ",∇num)
+#  @show ∇ad[k] - ∇num
+#end
+#
+#
+#
+#
+#
--- test/autodiff.jl
+++ test/autodiff.jl
@@ -4,321 +4,321 @@ using Test
 using LinearAlgebra
 using Random
 
-#@testset "one-qubit gate gradients" begin 
-#  Random.seed!(1234)
-#  N = 1
-#  ϵ = 1E-6
-#  
-#  q = qubits(N)
-#  i = q[1]
-#  ψ = productstate(q)
-#  
-#  # Rx
-#  θ = π/3
-#  for σ′ in [0,1]
-#    for σ in [0,1]
-#      bra = state(i,σ′+1) 
-#      ket = state(i,σ+1)
-#      function loss(θ)
-#        u = gate(ψ,"Rx",1, (θ = θ,))
-#        return abs((bra' * u * ket)[])
-#      end
-#      ∇num = (loss(θ+ϵ) - loss(θ))/(ϵ)
-#      @test ∇num ≈ loss'(θ) atol = 1e-5
-#    end
-#  end
-#  
-#  # Ry
-#  θ = π/3
-#  for σ′ in [0,1]
-#    for σ in [0,1]
-#      bra = state(i,σ′+1) 
-#      ket = state(i,σ+1)
-#      function loss(θ)
-#        u = gate(ψ,"Ry",1, (θ = θ,))
-#        return abs((bra' * u * ket)[])
-#      end
-#      ∇num = (loss(θ+ϵ) - loss(θ))/(ϵ)
-#      @test ∇num ≈ loss'(θ) atol = 1e-5
-#    end
-#  end
-#  
-#  # Rz
-#  ϕ = π/3
-#  for σ′ in [0,1]
-#    for σ in [0,1]
-#      bra = state(i,σ′+1) 
-#      ket = state(i,σ+1)
-#      function loss(ϕ)
-#        u = gate(ψ,"Rz",1, (ϕ = ϕ,))
-#        return abs((bra' * u * ket)[])
-#      end
-#      ∇num = (loss(ϕ+ϵ) - loss(ϕ))/(ϵ)
-#      @test ∇num ≈ loss'(ϕ) atol = 1e-5
-#    end
-#  end
-#  
-#  
-#  # Rn
-#  # XXX: wrong gradients, factor of two in the grad against θ
-#  θ = π/5
-#  ϕ = 0.13
-#  λ = 0.34
-#  for σ′ in [0,1]
-#    for σ in [0,1]
-#      bra = state(i,σ′+1) 
-#      ket = state(i,σ+1)
-#      function loss(pars)
-#        θ, ϕ, λ = pars
-#        u = gate(ψ,"Rn", 1, (θ = θ, ϕ = ϕ, λ = λ))
-#        return imag((bra' * u * ket)[])
-#      end
-#      p  = [θ,ϕ,λ]
-#      p₊ = [θ+ϵ, ϕ, λ]
-#      ∇θ = (loss(p₊) - loss(p))/ϵ
-#      p₊ = [θ, ϕ+ϵ, λ]
-#      ∇ϕ = (loss(p₊) - loss(p))/ϵ
-#      p₊ = [θ, ϕ, λ+ϵ]
-#      ∇λ = (loss(p₊) - loss(p))/ϵ
-#      
-#      ∇ = loss'([θ,ϕ,λ])
-#      @test ∇θ ≈ ∇[1] atol = 1e-5  
-#      @test ∇ϕ ≈ ∇[2] atol = 1e-5
-#      @test ∇λ ≈ ∇[3] atol = 1e-5
-#    end
-#  end
-#  
-#end
-#
-#
-#
-#@testset "two-qubit gate gradients" begin 
-#  Random.seed!(1234)
-#  N = 2
-#  ϵ = 1E-8
-#  basis = [[0,0],[0,1],[1,0],[1,1]]
-#  
-#  q = qubits(N)
-#  ψ = productstate(q)
-#  
-#  
-#  # CRy
-#  θ = π/2
-#  # sweep the matrix row by row
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
-#      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
-#      function loss(θ)
-#        U = gate(ψ,"CRy",(1,2), (θ = θ, f = x -> x))
-#        return (bra' * U * ket)[]
-#      end
-#      ∇num = (loss(θ+ϵ) - loss(θ-ϵ))/(2ϵ)
-#      @test ∇num ≈ loss'(θ)
-#    end
-#  end
-#  
-#  # CRz
-#  ϕ = π/3
-#  # sweep the matrix row by row
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
-#      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
-#      function loss(ϕ)
-#        U = gate(ψ,"CRz",(2,1), (ϕ = ϕ,))
-#        return abs((bra' * U * ket)[])
-#      end
-#      ∇num = (loss(ϕ+ϵ) - loss(ϕ-ϵ))/(2ϵ)
-#      @test ∇num ≈ loss'(ϕ)
-#    end
-#  end
-#  # CRy
-#  θ = π/2
-#  # sweep the matrix row by row
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
-#      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
-#      function loss(θ)
-#        U = gate(ψ,"CRy",(2,1), (θ = θ, f = x -> x))
-#        return (bra' * U * ket)[]
-#      end
-#      ∇num = (loss(θ+ϵ) - loss(θ-ϵ))/(2ϵ)
-#      @test ∇num ≈ loss'(θ)
-#    end
-#  end
-#  
-#  # CRz
-#  ϕ = π/3
-#  # sweep the matrix row by row
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
-#      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
-#      function loss(ϕ)
-#        U = gate(ψ,"CRz",(1,2), (ϕ = ϕ,))
-#        return abs((bra' * U * ket)[])
-#      end
-#      ∇num = (loss(ϕ+ϵ) - loss(ϕ-ϵ))/(2ϵ)
-#      @test ∇num ≈ loss'(ϕ)
-#    end
-#  end
-#  
-#  # Rn
-#  θ = π/5
-#  ϕ = π/3
-#  λ = π/7
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
-#      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
-#      function loss(pars)
-#        θ, ϕ, λ=pars
-#        u = gate(ψ,"CRn", (1,2), (θ = θ, ϕ = ϕ, λ = λ))
-#        return abs((bra' * u * ket)[])
-#      end
-#      p  = [θ,ϕ,λ]
-#      p₊ = [θ+ϵ, ϕ, λ]
-#      ∇θ = (loss(p₊) - loss(p))/ϵ
-#      p₊ = [θ, ϕ+ϵ, λ]
-#      ∇ϕ = (loss(p₊) - loss(p))/ϵ
-#      p₊ = [θ, ϕ, λ+ϵ]
-#      ∇λ = (loss(p₊) - loss(p))/ϵ
-#      
-#      ∇ = loss'([θ,ϕ,λ])
-#      @test ∇θ ≈ ∇[1] 
-#      @test ∇ϕ ≈ ∇[2] 
-#      @test ∇λ ≈ ∇[3] 
-#    end
-#  end
-#  
-#  # Rn
-#  θ = π/5
-#  ϕ = π/3
-#  λ = π/7
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
-#      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
-#      function loss(pars)
-#        θ, ϕ, λ=pars
-#        u = gate(ψ,"CRn", (2,1), (θ = θ, ϕ = ϕ, λ = λ))
-#        return abs((bra' * u * ket)[])
-#      end
-#      p  = [θ,ϕ,λ]
-#      p₊ = [θ+ϵ, ϕ, λ]
-#      ∇θ = (loss(p₊) - loss(p))/ϵ
-#      p₊ = [θ, ϕ+ϵ, λ]
-#      ∇ϕ = (loss(p₊) - loss(p))/ϵ
-#      p₊ = [θ, ϕ, λ+ϵ]
-#      ∇λ = (loss(p₊) - loss(p))/ϵ
-#      
-#      ∇ = loss'([θ,ϕ,λ])
-#      @test ∇θ ≈ ∇[1] 
-#      @test ∇ϕ ≈ ∇[2] 
-#      @test ∇λ ≈ ∇[3] 
-#    end
-#  end
-#  
-#  # Rxx
-#  ϕ = π/3
-#  # sweep the matrix row by row
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
-#      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
-#      function loss(ϕ)
-#        U = gate(ψ,"Rxx",(1,2), (ϕ = ϕ,))
-#        return abs((bra' * U * ket)[])
-#      end
-#      ∇num = (loss(ϕ+ϵ) - loss(ϕ-ϵ))/(2ϵ)
-#      @test ∇num ≈ loss'(ϕ)
-#    end
-#  end
-#
-#end
-#
-#
-#
-#
-#
-#
-#
-#
+@testset "one-qubit gate gradients" begin 
+  Random.seed!(1234)
+  N = 1
+  ϵ = 1E-6
+  
+  q = qubits(N)
+  i = q[1]
+  ψ = productstate(q)
+  
+  # Rx
+  θ = π/3
+  for σ′ in [0,1]
+    for σ in [0,1]
+      bra = state(i,σ′+1) 
+      ket = state(i,σ+1)
+      function loss(θ)
+        u = gate(ψ,"Rx",1, (θ = θ,))
+        return abs((bra' * u * ket)[])
+      end
+      ∇num = (loss(θ+ϵ) - loss(θ))/(ϵ)
+      @test ∇num ≈ loss'(θ) atol = 1e-5
+    end
+  end
+  
+  # Ry
+  θ = π/3
+  for σ′ in [0,1]
+    for σ in [0,1]
+      bra = state(i,σ′+1) 
+      ket = state(i,σ+1)
+      function loss(θ)
+        u = gate(ψ,"Ry",1, (θ = θ,))
+        return abs((bra' * u * ket)[])
+      end
+      ∇num = (loss(θ+ϵ) - loss(θ))/(ϵ)
+      @test ∇num ≈ loss'(θ) atol = 1e-5
+    end
+  end
+  
+  # Rz
+  ϕ = π/3
+  for σ′ in [0,1]
+    for σ in [0,1]
+      bra = state(i,σ′+1) 
+      ket = state(i,σ+1)
+      function loss(ϕ)
+        u = gate(ψ,"Rz",1, (ϕ = ϕ,))
+        return abs((bra' * u * ket)[])
+      end
+      ∇num = (loss(ϕ+ϵ) - loss(ϕ))/(ϵ)
+      @test ∇num ≈ loss'(ϕ) atol = 1e-5
+    end
+  end
+  
+  
+  # Rn
+  # XXX: wrong gradients, factor of two in the grad against θ
+  θ = π/5
+  ϕ = 0.13
+  λ = 0.34
+  for σ′ in [0,1]
+    for σ in [0,1]
+      bra = state(i,σ′+1) 
+      ket = state(i,σ+1)
+      function loss(pars)
+        θ, ϕ, λ = pars
+        u = gate(ψ,"Rn", 1, (θ = θ, ϕ = ϕ, λ = λ))
+        return imag((bra' * u * ket)[])
+      end
+      p  = [θ,ϕ,λ]
+      p₊ = [θ+ϵ, ϕ, λ]
+      ∇θ = (loss(p₊) - loss(p))/ϵ
+      p₊ = [θ, ϕ+ϵ, λ]
+      ∇ϕ = (loss(p₊) - loss(p))/ϵ
+      p₊ = [θ, ϕ, λ+ϵ]
+      ∇λ = (loss(p₊) - loss(p))/ϵ
+      
+      ∇ = loss'([θ,ϕ,λ])
+      @test ∇θ ≈ ∇[1] atol = 1e-5  
+      @test ∇ϕ ≈ ∇[2] atol = 1e-5
+      @test ∇λ ≈ ∇[3] atol = 1e-5
+    end
+  end
+  
+end
 
-#@testset "functions applied to qudit gates" begin 
-#  Random.seed!(1234)
-#  N = 2
-#  ϵ = 1E-8
-#  q = qudits(N; dim = 3)
-#  ψ = productstate(q)
-#  basis = [0,1,2]
-#  
-#  g(θ) = θ^2
-#  
-#  θ = π/3
-#  i = q[1]
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(i,σ′+1)
-#      ket = state(i,σ+1)
-#      function loss(θ)
-#        u = gate(ψ, "a", 1, (f = x ->  exp(im * g(θ) * x),))
-#        return abs((bra' * u * ket)[])
-#      end
-#      ∇num = (loss(θ+ϵ) - loss(θ))/ϵ
-#      @test ∇num ≈ loss'(θ) atol = 1e-5
-#    end
-#  end
-#
-#  θ = π/3
-#  i = q[1]
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(i,σ′+1)
-#      ket = state(i,σ+1)
-#      function loss(θ)
-#        u = gate(ψ, "a† * a", 1, (f = x ->  exp(im * g(θ) * x),))
-#        return abs((bra' * u * ket)[])
-#      end
-#      ∇num = (loss(θ+ϵ) - loss(θ))/ϵ
-#      @test ∇num ≈ loss'(θ) atol = 1e-5
-#    end
-#  end
-#  
-#  θ = π/3
-#  i = q[1]
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(i,σ′+1)
-#      ket = state(i,σ+1)
-#      function loss(θ)
-#        u = gate(ψ, "a† * a + a * a†", 1, (f = x ->  exp(im * g(θ) * x),))
-#        return abs((bra' * u * ket)[])
-#      end
-#      ∇num = (loss(θ+ϵ) - loss(θ))/ϵ
-#      @test ∇num ≈ loss'(θ) atol = 1e-5
-#    end
-#  end
-#
-#  basis = vec(Iterators.product(fill([0,1,2],2)...)|>collect)
-#  θ = π/3
-#  i = q[1]
-#  for σ′ in basis
-#    for σ in basis
-#      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
-#      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
-#      function loss(θ)
-#        u = gate(ψ, "aa†", (1,2), (f = x ->  exp(im * g(θ) * x),))
-#        return abs((bra' * u * ket)[])
-#      end
-#      ∇num = (loss(θ+ϵ) - loss(θ))/ϵ
-#      @test ∇num ≈ loss'(θ) atol = 1e-5
-#    end
-#  end
-#end
-#
+
+
+@testset "two-qubit gate gradients" begin 
+  Random.seed!(1234)
+  N = 2
+  ϵ = 1E-8
+  basis = [[0,0],[0,1],[1,0],[1,1]]
+  
+  q = qubits(N)
+  ψ = productstate(q)
+  
+  
+  # CRy
+  θ = π/2
+  # sweep the matrix row by row
+  for σ′ in basis
+    for σ in basis
+      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
+      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
+      function loss(θ)
+        U = gate(ψ,"CRy",(1,2), (θ = θ, f = x -> x))
+        return (bra' * U * ket)[]
+      end
+      ∇num = (loss(θ+ϵ) - loss(θ-ϵ))/(2ϵ)
+      @test ∇num ≈ loss'(θ)
+    end
+  end
+  
+  # CRz
+  ϕ = π/3
+  # sweep the matrix row by row
+  for σ′ in basis
+    for σ in basis
+      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
+      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
+      function loss(ϕ)
+        U = gate(ψ,"CRz",(2,1), (ϕ = ϕ,))
+        return abs((bra' * U * ket)[])
+      end
+      ∇num = (loss(ϕ+ϵ) - loss(ϕ-ϵ))/(2ϵ)
+      @test ∇num ≈ loss'(ϕ)
+    end
+  end
+  # CRy
+  θ = π/2
+  # sweep the matrix row by row
+  for σ′ in basis
+    for σ in basis
+      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
+      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
+      function loss(θ)
+        U = gate(ψ,"CRy",(2,1), (θ = θ, f = x -> x))
+        return (bra' * U * ket)[]
+      end
+      ∇num = (loss(θ+ϵ) - loss(θ-ϵ))/(2ϵ)
+      @test ∇num ≈ loss'(θ)
+    end
+  end
+  
+  # CRz
+  ϕ = π/3
+  # sweep the matrix row by row
+  for σ′ in basis
+    for σ in basis
+      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
+      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
+      function loss(ϕ)
+        U = gate(ψ,"CRz",(1,2), (ϕ = ϕ,))
+        return abs((bra' * U * ket)[])
+      end
+      ∇num = (loss(ϕ+ϵ) - loss(ϕ-ϵ))/(2ϵ)
+      @test ∇num ≈ loss'(ϕ)
+    end
+  end
+  
+  # Rn
+  θ = π/5
+  ϕ = π/3
+  λ = π/7
+  for σ′ in basis
+    for σ in basis
+      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
+      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
+      function loss(pars)
+        θ, ϕ, λ=pars
+        u = gate(ψ,"CRn", (1,2), (θ = θ, ϕ = ϕ, λ = λ))
+        return abs((bra' * u * ket)[])
+      end
+      p  = [θ,ϕ,λ]
+      p₊ = [θ+ϵ, ϕ, λ]
+      ∇θ = (loss(p₊) - loss(p))/ϵ
+      p₊ = [θ, ϕ+ϵ, λ]
+      ∇ϕ = (loss(p₊) - loss(p))/ϵ
+      p₊ = [θ, ϕ, λ+ϵ]
+      ∇λ = (loss(p₊) - loss(p))/ϵ
+      
+      ∇ = loss'([θ,ϕ,λ])
+      @test ∇θ ≈ ∇[1] 
+      @test ∇ϕ ≈ ∇[2] 
+      @test ∇λ ≈ ∇[3] 
+    end
+  end
+  
+  # Rn
+  θ = π/5
+  ϕ = π/3
+  λ = π/7
+  for σ′ in basis
+    for σ in basis
+      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
+      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
+      function loss(pars)
+        θ, ϕ, λ=pars
+        u = gate(ψ,"CRn", (2,1), (θ = θ, ϕ = ϕ, λ = λ))
+        return abs((bra' * u * ket)[])
+      end
+      p  = [θ,ϕ,λ]
+      p₊ = [θ+ϵ, ϕ, λ]
+      ∇θ = (loss(p₊) - loss(p))/ϵ
+      p₊ = [θ, ϕ+ϵ, λ]
+      ∇ϕ = (loss(p₊) - loss(p))/ϵ
+      p₊ = [θ, ϕ, λ+ϵ]
+      ∇λ = (loss(p₊) - loss(p))/ϵ
+      
+      ∇ = loss'([θ,ϕ,λ])
+      @test ∇θ ≈ ∇[1] 
+      @test ∇ϕ ≈ ∇[2] 
+      @test ∇λ ≈ ∇[3] 
+    end
+  end
+  
+  # Rxx
+  ϕ = π/3
+  # sweep the matrix row by row
+  for σ′ in basis
+    for σ in basis
+      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
+      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
+      function loss(ϕ)
+        U = gate(ψ,"Rxx",(1,2), (ϕ = ϕ,))
+        return abs((bra' * U * ket)[])
+      end
+      ∇num = (loss(ϕ+ϵ) - loss(ϕ-ϵ))/(2ϵ)
+      @test ∇num ≈ loss'(ϕ)
+    end
+  end
+
+end
+
+
+
+
+
+
+
+
+
+@testset "functions applied to qudit gates" begin 
+  Random.seed!(1234)
+  N = 2
+  ϵ = 1E-8
+  q = qudits(N; dim = 3)
+  ψ = productstate(q)
+  basis = [0,1,2]
+  
+  g(θ) = θ^2
+  
+  θ = π/3
+  i = q[1]
+  for σ′ in basis
+    for σ in basis
+      bra = state(i,σ′+1)
+      ket = state(i,σ+1)
+      function loss(θ)
+        u = gate(ψ, "a", 1, (f = x ->  exp(im * g(θ) * x),))
+        return abs((bra' * u * ket)[])
+      end
+      ∇num = (loss(θ+ϵ) - loss(θ))/ϵ
+      @test ∇num ≈ loss'(θ) atol = 1e-5
+    end
+  end
+
+  θ = π/3
+  i = q[1]
+  for σ′ in basis
+    for σ in basis
+      bra = state(i,σ′+1)
+      ket = state(i,σ+1)
+      function loss(θ)
+        u = gate(ψ, "a† * a", 1, (f = x ->  exp(im * g(θ) * x),))
+        return abs((bra' * u * ket)[])
+      end
+      ∇num = (loss(θ+ϵ) - loss(θ))/ϵ
+      @test ∇num ≈ loss'(θ) atol = 1e-5
+    end
+  end
+  
+  θ = π/3
+  i = q[1]
+  for σ′ in basis
+    for σ in basis
+      bra = state(i,σ′+1)
+      ket = state(i,σ+1)
+      function loss(θ)
+        u = gate(ψ, "a† * a + a * a†", 1, (f = x ->  exp(im * g(θ) * x),))
+        return abs((bra' * u * ket)[])
+      end
+      ∇num = (loss(θ+ϵ) - loss(θ))/ϵ
+      @test ∇num ≈ loss'(θ) atol = 1e-5
+    end
+  end
+
+  basis = vec(Iterators.product(fill([0,1,2],2)...)|>collect)
+  θ = π/3
+  i = q[1]
+  for σ′ in basis
+    for σ in basis
+      bra = state(q[1],σ′[1]+1) * state(q[2], σ′[2]+1)
+      ket = state(q[1],σ[1]+1)  * state(q[2], σ[2]+1)
+      function loss(θ)
+        u = gate(ψ, "aa†", (1,2), (f = x ->  exp(im * g(θ) * x),))
+        return abs((bra' * u * ket)[])
+      end
+      ∇num = (loss(θ+ϵ) - loss(θ))/ϵ
+      @test ∇num ≈ loss'(θ) atol = 1e-5
+    end
+  end
+end
+
 
 @testset "trotter - 1-qubit gate" begin
   N = 4
