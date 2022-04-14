20e556937ae7266515b96ef2d5b81a338b895f71
Matthew Fishman <mfishman@flatironinstitute.org>
2022-04-14 18:31:40 -0400

    Bring back explicit test list

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,33 @@
 using Test
 
+# TODO: replace with:
+#
+# filenames = readdir()
+#
+# However this seems to cause some issues with some process
+# tomography tests, it seems to be sensitive to the ordering
+# they are called.
+filenames = [
+  "test_array.jl",
+  "test_autodiff.jl",
+  "test_circuits.jl",
+  "test_distances.jl",
+  "test_fulltomography.jl",
+  "test_gates.jl",
+  "test_getsamples.jl",
+  "test_io.jl",
+  "test_noise.jl",
+  "test_optimizers.jl",
+  "test_processtomography.jl",
+  "test_productstates.jl",
+  "test_qubitarrays.jl",
+  "test_randomstates.jl",
+  "test_runcircuit.jl",
+  "test_gpu.jl",
+  "test_statetomography.jl",
+  "test_utils.jl",
+]
+
 @testset "PastaQ.jl" begin
   @testset "$filename" for filename in readdir()
     if startswith(filename, "test_") && endswith(filename, ".jl")
